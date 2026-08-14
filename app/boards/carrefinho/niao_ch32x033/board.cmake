77168bb0474549b15368194a4599e59a50d546c9
carrefinho <carrefinho@proton.me>
2026-08-14 05:43:05 -0400

    Revert "boards: add the Niao CH32X033"
    This reverts commit f0d87d515218393c41fee14295aca69cb9a8721a.

--- app/boards/carrefinho/niao_ch32x033/board.cmake
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026 The ZMK Contributors
-# SPDX-License-Identifier: MIT
-
-board_runner_args(minichlink "--dt-flash=y")
-board_runner_args(wlink "--chip=CH32X035")
-board_runner_args(wchisp)
-include(${ZEPHYR_BASE}/boards/common/minichlink.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/wlink.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/wchisp.board.cmake)
